1008e035ff589baa95500400402a0b3bca815ea8
Hesam Asnaashari <ilhesam@outlook.com>
2020-10-29 23:43:53 +0330

    Add 'BprDbSet'

---
+++ BPR.AspNetCore.EF/Base/BprDbSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BPR.AspNetCore.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BPR.AspNetCore.EF
+{
+    public class BprDbSet<TEntity> : DbSet<TEntity>
+        where TEntity : BaseEntity
+    {
+    }
+}
