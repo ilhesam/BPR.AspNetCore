2b4ef579b9328ace92f2fe337eecfd51c6a47373
Hesam Asnaashari <ilhesam@outlook.com>
2020-10-31 04:36:10 +0330

    Add 'IBprMapper'

---
+++ BPR.AspNetCore.Service/Base/IBprMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BPR.AspNetCore.Entity;
+
+namespace BPR.AspNetCore.Service.Base
+{
+    public interface IBprMapper<out TEntity, out TEntityAddDto, out TEntityEditDto, out TEntityDto, out TEntitiesDto>
+        where TEntity : BaseEntity
+        where TEntityAddDto : BaseAddDto
+        where TEntityEditDto : BaseEditDto
+        where TEntityDto : BaseEntityDto
+        where TEntitiesDto : BaseEntitiesDto<TEntityDto>
+    {
+        TEntity MapToEntity(object obj);
+        TEntityAddDto MapToAddDto(object obj);
+        TEntityEditDto MapToEditDto(object obj);
+        TEntityDto MapToEntityDto(object obj);
+        TEntitiesDto MapToEntitiesDto(object obj);
+    }
+}
